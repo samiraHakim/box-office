e65fe57f4d2f4b7c26130b7f5ce84fa08b5bfa13
Samira <ruksanahakim86@gmail.com>
2022-07-23 00:35:02 +0530

    replaced usestate with useReducer

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,13 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
 // eslint-disable-next-line react/function-component-definition
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+
+    default:
+      return prevState;
+  }
+};
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
+// eslint-disable-next-line react/function-component-definition
 const Show = () => {
   const { id } = useParams();
-  const [show, setShow] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   useEffect(() => {
     let isMounted = true;
@@ -15,15 +35,13 @@ const Show = () => {
       .then(results => {
         setTimeout(() => {
           if (isMounted) {
-            setShow(results);
-            setIsLoading(false);
+            dispatch({ type: 'FETCH_SUCCESS', show: results });
           }
         });
       })
       .catch(err => {
         if (isMounted) {
-          setError(err.message);
-          setIsLoading(false);
+          dispatch({ type: 'FFETCH_FAILED', error: err.message });
         }
       });
     return () => {
